993bab4c92c37f381c061c47664695f12cb63d37
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2024-10-28 19:59:15 +0000

    estilos arreglados

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
+import Swal from 'sweetalert2'
 
 const Home = () => {
 
@@ -15,17 +16,23 @@ const Home = () => {
 	// Eliminar una tarea por índice
 	const deleteTask = (index) => {
 		setTasks(tasks.filter((_, i) => i !== index));
+
+		Swal.fire({
+			title: "Bien hecho!",
+			text: "Has terminado una tarea!",  // alerta sacada de sweetAlert, hay que instalar el npm e importar sweetAlert
+			icon: "success"
+		  });
 	};
 
 	return (
 		<>
-			<div className="card justify-content-center align-items-center shadow col-6 mx-auto mt-5">
-				<h1 className="card-title mb-3">
+			<div className="card justify-content-center align-items-center shadow-lg col-6 mx-auto mt-5">
+				<h1 className="card-title mb-3 mt-5 dynapuff">
 					Lista de Tareas
 				</h1>
 
 				<input
-					className="text-center"
+					className="text-center rounded"
 					type="text"
 					placeholder="Añade una nueva tarea"
 					value={newTask}
@@ -33,13 +40,13 @@ const Home = () => {
 				/>
 				<button
 					type="button"
-					className="btn btn-outline-secondary mt-2"
+					className="btn btn-outline-secondary mt-4"
 					onClick={addTask}>
 					<i className="fas fa-plus-circle me-2"></i>
 					Agregar
 				</button>
 				<TodoList tasks={tasks} deleteTask={deleteTask} />
-				<p>{tasks.length} Tareas pendientes</p>
+				<p className="dancing-script mb-5">{tasks.length} Tareas pendientes</p>
 			</div>
 
 		</>
